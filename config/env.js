d857a61977c2837c3b4d71e3340c9dc4099c7f5e
Dn Vidya <anish@ferventsoft.com>
2019-07-11 16:54:26 +0530

    few chages updated successfully...

--- config/env.js
+++ config/env.js
@@ -10,6 +10,6 @@ export const SMTP_HOST='smtp.gmail.com';
 export const SMTP_PORT=465;
 export const SMTP_USERNAME='munna.bhakta1001@gmail.com';
 export const SMTP_PASSWORD='rrguuftkqwdoxuav';
-export const SMTP_BccEmail='joabkr@gmail.com,sufinoon@gmail.com';
+export const PRO_ENQUIRY_BccEmail='joabkr@gmail.com,sufinoon@gmail.com';
 
 export const invitationLink="http://signal-in.com/spotlight/check/";
