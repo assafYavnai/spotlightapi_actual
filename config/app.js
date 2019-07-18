761f3c70cb76c62be90cde5abfb3520459f7c857
Munna Bhakta <munna@ferventsoft.com>
2019-07-18 18:54:54 +0530

    Fix email issue

--- config/app.js
+++ config/app.js
@@ -5,6 +5,6 @@ export const isDebug = ENV === 'development';
 export const isClient = typeof window !== 'undefined';
 
 export const hostName = isDebug ? 'http://localhost:3000' : 'http://122.160.166.186:5000';
-export const privateLocalAddress = isDebug ? 'http://localhost:5000' : 'http://192.168.1.101:5000';
+export const privateLocalAddress = isDebug ? 'http://localhost:5000' : 'http://192.168.1.101:5001';
 
 
