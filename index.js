e58d63ab1d7fd8fb0f5e8d42de7e6280bbf18756
Munna Bhakta <Munna@ferventsoft.com>
2019-08-03 14:59:43 +0530

    SPOT-45 #comment improvment #time 40m

--- index.js
+++ index.js
@@ -63,7 +63,12 @@ app.use(expressWinston.logger({
     msg: "HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
     expressFormat: false, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
     colorize: false, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
-    ignoreRoute: function (req, res) { return false; } // optional: allows to skip some log messages based on request and/or response
+    ignoreRoute: function (req, res) {
+        if(req.url=='/api'){
+            return true;
+        } 
+        return false;
+     } // optional: allows to skip some log messages based on request and/or response
   }));   
 /*
  * REMOVE if you do not need any routes
