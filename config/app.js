cd921b88468fea01dde545e6aa8b3dda1fee364a
Munna Bhakta <munna@ferventsoft.com>
2019-11-19 14:04:23 +0530

    Fix compilation error

--- config/app.js
+++ config/app.js
@@ -1,9 +1,9 @@
-import { ENV,hostName,privateLocalAddress } from './env';
+import { ENV,hostName as host,privateLocalAddress as localHost } from './env';
 
 export const isProduction = ENV == 'production';
 export const isDebug = ENV == 'development';
 export const isClient = typeof window !== 'undefined';
-export const hostName = hostName;
-export const privateLocalAddress = privateLocalAddress;
+export const hostName = host;
+export const privateLocalAddress = localHost;
 
 
