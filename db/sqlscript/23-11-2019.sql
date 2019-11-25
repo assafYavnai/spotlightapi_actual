97d86b5048ac6a12b37a64cb0513c7f01bded969
Dn Vidya <anish@ferventsoft.com>
2019-11-25 12:46:08 +0530

    SPOT-60 #comment Add sendInvitation modification table new script has been updated ..#time 10m

--- db/sqlscript/23-11-2019.sql
+++ db/sqlscript/23-11-2019.sql
@@ -1,11 +1,10 @@
 -- This script for sendInvitation table altering->Removing the primary_key from id column    and apply the multiple column(email,user_check_id).
-
--- Remove Existing PrimaryKey
+--Delete primary Key 
 alter table user_check_invitations drop constraint user_check_invitations_pkey ;
-
---add multiple pkey(email,user_check_id)
-
+--delete user_check_id null values
+DELETE from user_check_invitations where user_check_id is null;
+--- add multiple primary key
 alter table user_check_invitations add constraint user_check_pkey primary key (email,user_check_id);
-
--- Delete Null Record of user_check_id
-DELETE FROM user_check_invitations WHERE user_check_id IN NULL 
+--- delete duplicate record of email and user_check_id
+DELETE FROM user_check_invitations where id in(SELECT id from (SELECT max(id) as id,user_check_id,count(email) from user_check_invitations group by user_check_id,email
+having count(email)>1) tbl);
