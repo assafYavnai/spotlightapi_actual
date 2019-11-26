bfe60e3e42fee7a621cfa7ca2933ec99866ac83e
Munna Bhakta <munna@ferventsoft.com>
2019-11-26 06:31:37 +0000

    Revert "Feature/customcheck (pull request #8)"
    This reverts pull request #8.
    
    > * SPOT-52 #comment added newly field in model for custom check #time 20m
    > * SPOT-51 #comment Add Custom check database inserted record as per document successfully updated
    > * SPOT-52 #comment created new api to fetch topics pool for custom check #time 2h
    > * Database table has been updated successfully..
    > 
    > ‌

--- db/sequelize/models/topics_master.js
+++ db/sequelize/models/topics_master.js
@@ -9,8 +9,7 @@ module.exports = (sequelize, DataTypes) => {
     description_he: DataTypes.STRING,
     is_active: DataTypes.BOOLEAN,
     estimated_time: DataTypes.INTEGER,
-    sequence:DataTypes.INTEGER,
-    child_category_id: DataTypes.INTEGER
+    sequence:DataTypes.INTEGER
   }, {});
   topics_master.associate = function(models) {
     // associations can be defined here
